637a684e26fafba8b5687175f13cd5c5f80b524c
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-04-12 19:46:27 +0900

    test

--- javascripts/sample.js
+++ javascripts/sample.js
@@ -1,6 +1,3 @@
-function getElement() {
-  const element = document.getElementById("title");
-  element.innerText = "idで取得した要素を変更しました"
-};
-
-window.onload = getElement;
+$(function(){
+  $("h1").removeClass("title");
+});
--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,5 +1,35 @@
-function confirmSubmit() {
-    // ここに処理を記述
-  };
-  
-  window.onload = confirmSubmit;
+function emailValidation() {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', function(e) {
+    if(form.email.value !== form.email_confirm.value) {
+      e.preventDefault();
+      const element = document.createElement('p')
+      const message = document.createTextNode("Eメールが一致しません")
+      form.appendChild(element);
+      element.appendChild(message);
+      element.classList.add("alert");
+      setTimeout(function() {
+        form.removeChild(element);
+      }, 3000);
+    }
+  });
+};
+
+functiuon inputEmailValidation(){
+  const form = document.getElementbyld('form');
+  form.addEventListner('email_confirm',function(e){
+    if(form.email.value !== form.email_confirm.value || form.email.value !== null){
+    e.preventDefault();
+    const element = document.createElement('p')
+    const message = document.createTextNode("Eメールが一致しません")
+    form.appendChild(element);
+    element.appendChild(message);
+    element.classList.add("input_alert");
+    }
+  });
+};
+
+window.onload = function{ 
+  emailValidation();
+  inputEmailValidation();
+}
