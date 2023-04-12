7574bfed808d82937f96bf75351665525b9d0184
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-04-12 20:53:54 +0900

    イベント種類の変更

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,6 +1,10 @@
 function emailValidation() {
   const form = document.getElementById('form');
-  form.addEventListener('submit', function(e) {
+  // NOTE: イベントを付与したいのはemail_confirmなので取得してイベントの付与先を変更する
+  const confirm = document.getElementById('email_confirm');
+
+  console.log(confirm);
+  confirm.addEventListener('input', function(e) {
     if(form.email.value !== form.email_confirm.value) {
       e.preventDefault();
       const element = document.createElement('p')
@@ -8,28 +12,12 @@ function emailValidation() {
       form.appendChild(element);
       element.appendChild(message);
       element.classList.add("alert");
-      setTimeout(function() {
-        form.removeChild(element);
-      }, 3000);
     }
   });
 };
 
-functiuon inputEmailValidation(){
-  const form = document.getElementbyld('form');
-  form.addEventListner('email_confirm',function(e){
-    if(form.email.value !== form.email_confirm.value || form.email.value !== null){
-    e.preventDefault();
-    const element = document.createElement('p')
-    const message = document.createTextNode("Eメールが一致しません")
-    form.appendChild(element);
-    element.appendChild(message);
-    element.classList.add("input_alert");
-    }
-  });
-};
 
-window.onload = function{ 
+
+window.onload = function(){ 
   emailValidation();
-  inputEmailValidation();
-}
+};
