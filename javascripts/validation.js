4a688bd1dd263ee209fe1f888c09e1a90f13f958
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-04-13 00:11:30 +0900

    css適用の変更

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -6,10 +6,12 @@ function emailValidation() {
   // TODO: 最初にエラー表示のラベルを追加してしまう
   const element = document.createElement('p')
   const message = document.createTextNode("Eメールが一致しません")
-  form.appendChild(element);
+  
+  // TOOD[宿題]: サンプルと同じ場所にエレメントを追加する
+  confirm.appendChild(element);
   element.appendChild(message);
   // 理想: この時点で非表示
-  element.classList.add("alert");
+  element.classList.add("invisible");
   console.log(confirm);
 
   confirm.addEventListener('input', function(e) {
@@ -17,12 +19,17 @@ function emailValidation() {
     if(form.email.value !== form.email_confirm.value) {
       e.preventDefault();
       // 理想: この時点で表示
+      element.classList.remove("invisible");
       element.classList.add("visible_alert");
+      element.classList.add("input_alert")
+      console.log(confirm);
     }
+    
     //  TODO: エラーがない場合には非表示にする
     else{
       // 理想: この時点で非表示
       element.classList.remove("visible_alert");
+      element.classList.remove("input_alert");
 
     }
   });
