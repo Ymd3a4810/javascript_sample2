e2f2b0cce47368181bb85773e3f4fd6e19ebf1b8
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-04-12 21:11:23 +0900

    CSSの変更

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -3,20 +3,30 @@ function emailValidation() {
   // NOTE: イベントを付与したいのはemail_confirmなので取得してイベントの付与先を変更する
   const confirm = document.getElementById('email_confirm');
 
+  // TODO: 最初にエラー表示のラベルを追加してしまう
+  const element = document.createElement('p')
+  const message = document.createTextNode("Eメールが一致しません")
+  form.appendChild(element);
+  element.appendChild(message);
+  // 理想: この時点で非表示
+  element.classList.add("alert");
   console.log(confirm);
+
   confirm.addEventListener('input', function(e) {
+    // TODO: エラーの場合には表示する
     if(form.email.value !== form.email_confirm.value) {
       e.preventDefault();
-      const element = document.createElement('p')
-      const message = document.createTextNode("Eメールが一致しません")
-      form.appendChild(element);
-      element.appendChild(message);
-      element.classList.add("alert");
+      // 理想: この時点で表示
+      element.classList.add("visible_alert");
+    }
+    //  TODO: エラーがない場合には非表示にする
+    else{
+      // 理想: この時点で非表示
+      element.classList.remove("visible_alert");
+
     }
   });
 };
-
-
 
 window.onload = function(){ 
   emailValidation();
